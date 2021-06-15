78b9d7fab52dcb5f4e692a86ff4a2cead3bd9d51
Hyunjae Kim <kimhun456@gmail.com>
2021-06-16 03:22:18 +0900

    Add BottomAppBar and FAB

--- app/src/main/java/com/github/kimhun456/memoapplication/presentation/main/MainActivity.kt
+++ app/src/main/java/com/github/kimhun456/memoapplication/presentation/main/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Surface
+import androidx.compose.material.FabPosition
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.github.kimhun456.memoapplication.presentation.theme.TheMemoTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -20,21 +24,54 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityTheme {
-                MainScreen(
-                    modifier = Modifier.padding(12.dp),
-                    viewModel = viewModel
-                )
+            TheMemoTheme {
+                MainContent()
             }
         }
     }
 
     @Composable
-    fun MainActivityTheme(content: @Composable () -> Unit) {
-        TheMemoTheme {
-            Surface {
-                content()
-            }
+    fun MainContent() {
+        val scaffoldState = rememberScaffoldState()
+        val scope = rememberCoroutineScope()
+        Scaffold(
+            modifier = Modifier.padding(8.dp),
+            scaffoldState = scaffoldState,
+            bottomBar = {
+                MainBottomAppBar(
+                    onClickMenuIcon = {
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Menu clicked")
+                        }
+                    },
+                    onClickSettingsIcon = {
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("setting clicked")
+                        }
+                    },
+                    onClickAddIcon = {
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("plus clicked")
+                        }
+                    }
+                )
+            },
+            floatingActionButton = {
+                WriteButton {
+                    viewModel.addMemo()
+                    scope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar("Add Generated Memo")
+                    }
+                }
+            },
+            isFloatingActionButtonDocked = true,
+            floatingActionButtonPosition = FabPosition.Center,
+        )
+        { innerPadding ->
+            MainScreen(
+                modifier = Modifier.padding(innerPadding),
+                viewModel = viewModel
+            )
         }
     }
 }
--- app/src/main/java/com/github/kimhun456/memoapplication/presentation/main/MainScreen.kt
+++ app/src/main/java/com/github/kimhun456/memoapplication/presentation/main/MainScreen.kt
@@ -2,14 +2,24 @@ package com.github.kimhun456.memoapplication.presentation.main
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Card
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -29,7 +39,6 @@ fun MainScreen(
     viewModel: MainViewModel
 ) {
     Column(modifier = modifier.fillMaxSize()) {
-        CreateRandomMemoButton(onClick = viewModel::addMemo)
         MemoList(viewModel)
     }
 }
@@ -43,16 +52,6 @@ fun MemoList(viewModel: MainViewModel) {
         items(items = memoList) { memo ->
             MemoCard(memo) { viewModel.removeMemo(memo) }
         }
-    }
-}
-
-@Composable
-fun CreateRandomMemoButton(onClick: () -> Unit) {
-    Button(
-        modifier = Modifier.fillMaxWidth(),
-        onClick = onClick
-    ) {
-        Text(text = "Create Random Memo")
     }
 }
 
@@ -87,11 +86,57 @@ fun MemoCard(
     }
 }
 
+@Composable
+fun MainBottomAppBar(
+    onClickMenuIcon: () -> Unit = {},
+    onClickSettingsIcon: () -> Unit = {},
+    onClickAddIcon: () -> Unit = {}
+) {
+    BottomAppBar(
+        cutoutShape = RoundedCornerShape(percent = 50)
+    ) {
+        IconButton(onClick = onClickMenuIcon) {
+            Icon(
+                Icons.Filled.Menu,
+                contentDescription = "Menu"
+            )
+        }
+        Spacer(Modifier.weight(1f, true))
+        IconButton(onClick = onClickSettingsIcon) {
+            Icon(
+                Icons.Filled.Settings,
+                contentDescription = "Settings"
+            )
+        }
+        IconButton(onClick = onClickAddIcon) {
+            Icon(
+                Icons.Filled.Add,
+                contentDescription = "Add"
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
-fun PreviewCreateMemoButton() {
+fun PreviewMainBottomAppBar() {
     TheMemoTheme {
-        CreateRandomMemoButton {
+        MainBottomAppBar()
+    }
+}
+
+@Composable
+fun WriteButton(onClick: () -> Unit) {
+    FloatingActionButton(onClick = onClick) {
+        Icon(Icons.Filled.Edit, contentDescription = "Write start")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewWriteButton() {
+    TheMemoTheme {
+        WriteButton {
 
         }
     }
